e5d32f8e97e26b853275cb8d21f1bcb0664fc335
masaru ogawa <masaruo@gmail.com>
2025-03-04 21:59:51 +0900

    draw.js deleted

--- frontend/index.js
+++ frontend/index.js
@@ -6,11 +6,13 @@ const x = canvas.width;
 const y = canvas.height;
 
 let ball = { x: x_center, y: y_center, dx: 5, dy: -5, radius: 10};
-// let left_paddle = { x: 0, y: y_center, width: 10, height: 80};
-let right_paddle = { x: x - 10, y: y_center - 40, width: 10, height: 80};
+let left_paddle = { x: 0, y: y_center, width: 20, height: 80};
+let right_paddle = { x: x - 20, y: y_center - 40, width: 20, height: 80};
 
 let r_up_pressed = false;
 let r_down_pressed = false;
+let l_up_pressed = false;
+let l_down_pressed = false;
 
 let intervalId = null;
 
@@ -27,7 +29,7 @@ document.getElementById("end").addEventListener('click', function(){
 
 function startGame(){
 	if (!intervalId){
-		intervalId = setInterval(draw, 10);
+		intervalId = setInterval(draw, 20);
 	}
 }
 
@@ -41,6 +43,10 @@ function keyDownHandler(e){
 		r_up_pressed = true;
 	else if (e.key == "ArrowDown")
 		r_down_pressed = true;
+	else if (e.key == "a")
+		l_up_pressed = true;
+	else if (e.key == "z")
+		l_down_pressed = true;
 }
 
 function keyUpHandler(e){
@@ -48,6 +54,10 @@ function keyUpHandler(e){
 		r_up_pressed = false;
 	else if (e.key == "ArrowDown")
 		r_down_pressed = false;
+	else if (e.key == "a")
+		l_up_pressed = false;
+	else if (e.key == "z")
+		l_down_pressed = false;
 }
 
 function drawBackGround(){
@@ -67,6 +77,8 @@ function drawPaddle(){
 	ctx.beginPath();
 	ctx.fillStyle = "green";
 	ctx.fillRect(right_paddle.x, right_paddle.y, right_paddle.width, right_paddle.height);
+	ctx.fillStyle = "blue";
+	ctx.fillRect(left_paddle.x, left_paddle.y, left_paddle.width, left_paddle.height);
 	ctx.closePath();
 }
 
@@ -74,6 +86,27 @@ function draw(){
 	ball.x += ball.dx;
 	ball.y += ball.dy;
 
+	// ボールが壁に当たった時
+	if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height){
+		ball.dy = ball.dy * -1;
+	}else if (ball.x - ball.radius < 0 || ball.x - ball.radius > canvas.width){
+		alert("GameOver");
+		document.location.reload();
+	}
+
+
+	// パドルとの接触
+	// right paddle
+	if (ball.x - ball.radius > canvas.width - 40 && ball.y - ball.radius > right_paddle.y && ball.y - ball.radius < right_paddle.y + right_paddle.height)
+	{
+		ball.dx = -Math.abs(ball.dx);
+	}
+	else if (ball.x - ball.radius < 20 && ball.y - ball.radius > left_paddle.y && ball.y - ball.radius < left_paddle.y + left_paddle.height)
+	{
+		ball.dx = Math.abs(ball.dx);
+	}
+
+	//右のパドルの上下
 	if (r_up_pressed && right_paddle.y > 0){
 		right_paddle.y -= 10;
 	}
@@ -81,57 +114,19 @@ function draw(){
 		right_paddle.y += 10;
 	}
 
+	//左のパドルの上下
+	if (l_up_pressed && left_paddle.y > 0){
+		left_paddle.y -= 10;
+	}
+	else if (l_down_pressed && left_paddle.y + left_paddle.height < canvas.height){
+		left_paddle.y += 10;
+	}
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBackGround();
 	drawBall();
 	drawPaddle();
 }
-
-// const right_paddle = canvas.getContext("2d");
-// const left_paddle = canvas.getContext("2d");
-// const ball = canvas.getContext("2d");
-
-// let right_paddle_y = 100;
-// let left_paddle_y = 200;
-
-// let ball_x = 100;
-// let ball_y = 299;
-
-// const right_paddle_up = document.getElementById("right-paddle-up");
-// const right_paddle_down = document.getElementById("right-paddle-down");
-
-// right_paddle.fillStyle = "blue";
-// right_paddle.fillRect(canvas.width - 10, right_paddle_y, 10, 100);
-
-// left_paddle.fillStyle = "green";
-// left_paddle.fillRect(0, left_paddle_y, 10, 100);
-
-// ball.fillStyle = "white";
-// ball.fillRect(ball_x, ball_y, 10, 10);
-
-// right_paddle_up.addEventListener("click", (e) => {
-// 	e.preventDefault();
-// 	right_paddle.fillStyle = "black";
-// 	right_paddle.fillRect(canvas.width - 10, right_paddle_y, 10, 100);
-// 	right_paddle_y -= 10;
-// 	if (right_paddle_y < 0)
-// 		right_paddle_y = 0;
-// 	right_paddle.fillStyle = "blue";
-// 	right_paddle.fillRect(canvas.width - 10, right_paddle_y, 10, 100);
-// })
-
-// right_paddle_down.addEventListener("click", (e) => {
-// 	e.preventDefault();
-// 	right_paddle.fillStyle = "black";
-// 	right_paddle.fillRect(canvas.width - 10, right_paddle_y, 10, 100);
-// 	right_paddle_y += 10;
-// 	if (right_paddle_y > canvas.height - 100)
-// 		right_paddle_y = canvas.height - 100;
-// 	console.log(right_paddle_y);
-// 	right_paddle.fillStyle = "blue";
-// 	right_paddle.fillRect(canvas.width - 10, right_paddle_y, 10, 100);
-// })
-
 
 
 // make shape AClass -> paddle, ball
