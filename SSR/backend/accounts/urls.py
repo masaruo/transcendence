df9736c6895275d1f9349affc0b7954c0d0e9daf
masaru ogawa <masaruo@gmail.com>
2025-03-06 13:47:18 +0900

    backend - before change project name

---
+++ SSR/backend/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+urlpatterns = [
+	path("login/", LoginView.as_view(
+		redirect_authenticated_user = True,
+		template_name = 'accounts/loging.html',
+	), name='login')
+]
