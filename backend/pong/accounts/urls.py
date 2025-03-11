c0de48eb7dbd3afb9984639bfa6a797494aee88d
masaru ogawa <masaruo@gmail.com>
2025-03-11 10:47:38 +0900

    backend folder all cleared to start from scratch

--- backend/pong/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
-
-urlpatterns = [
-	path('login/', LoginView.as_view(
-		redirect_authenticated_user = True,
-		template_name = 'accounts/login.html',
-	), name='login'),
-]
