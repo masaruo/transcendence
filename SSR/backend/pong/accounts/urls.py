97fad50c9dd3585247f84c78fec7fc7f9a851980
masaru ogawa <masaruo@gmail.com>
2025-03-11 10:43:36 +0900

    start of user mgmt

--- SSR/backend/pong/accounts/urls.py
+++ SSR/backend/pong/accounts/urls.py
@@ -2,8 +2,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 urlpatterns = [
-	path("login/", LoginView.as_view(
+	path('login/', LoginView.as_view(
 		redirect_authenticated_user = True,
-		template_name = 'accounts/loging.html',
-	), name='login')
+		template_name = 'accounts/login.html',
+	), name='login'),
 ]
