07f62d3b93d8b0940bc17832d4f86fb71772ce65
masaru ogawa <masaruo@gmail.com>
2025-03-12 15:03:20 +0900

    JWT implemented

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -9,7 +9,9 @@ from drf_spectacular.views import (
 from rest_framework_simplejwt.views  import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,5 +19,6 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name="token_verfify"),
     path('api/user/', include("user.urls")),
 ]
