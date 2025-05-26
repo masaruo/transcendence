7d1ea67fd3354b8f1029d0856a1d9acd15f0baf2
masaru ogawa <masaruo@gmail.com>
2025-05-26 18:00:51 +0900

    websocket path to WSS

--- frontend/src/services/constants.ts
+++ frontend/src/services/constants.ts
@@ -1,3 +1,3 @@
 // export const PATH: string = "http://localhost:8000";
 export const PATH: string = "https://localhost";
-export const WS_PATH: string = "localhost:8000";
+export const WS_PATH: string = "wss://localhost";
