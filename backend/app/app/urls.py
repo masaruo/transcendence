f884f365206dd1f39ed45f6e64b4b51901364c7c
masaru ogawa <masaruo@gmail.com>
2025-05-27 15:32:37 +0900

    not using files deleted

--- backend/app/app/urls.py
+++ backend/app/app/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from app.views import serve_media
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -16,7 +15,6 @@ from rest_framework_simplejwt.views import (
 )
 
 from debug_toolbar.toolbar import debug_toolbar_urls
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
